6621d30f4dc1a0ebb318401858beb54e8586f469
ceda-dev-01 <l.lezzerini@coopceda.it>
2022-04-27 12:14:29 +0200

    Prima versione

---
+++ src/main/java/com/iad/eserciziomovimenti/Eserciziomovimenti.java
@@ -0,0 +1,15 @@
+/*
+ */
+
+package com.iad.eserciziomovimenti;
+
+/**
+ *
+ * @author use
+ */
+public class Eserciziomovimenti {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
